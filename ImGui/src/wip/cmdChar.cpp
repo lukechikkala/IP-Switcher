863230cc70dcca8891bba95f5a08fc88ae9a4ca3
Luke Chikkala <lukechikkala@gmail.com>
2022-04-13 00:03:31 +0800

    Alrighty; Off to Italy now :)

--- ImGui/src/wip/cmdChar.cpp
+++ ImGui/src/wip/cmdChar.cpp
@@ -15,21 +15,39 @@ int main()
 		perror("ERROR");
 
 	std::string Networks;																// "Networks" of string data type
-	// for (int i = 0 ; i < 20 ; i++)
-	// {
-	// 	std::cout << Networks[i] << std::endl;													// NIC's sample Output
-	// 	// if (NICs[i] == '\n')
-	// 	// 	std::cout << "BREAK" << std::endl;
-	// }
 	Networks += NICs;
-	int Network_Size = sizeof(NICs);	// 218 at the moment
-	// std::cout << Networks << std::endl;
-	char val1 = 'A';
-	char val2 = 'b';
+/*
+############################################################
+WORKING EXAMPLE
+############################################################
+*/
+	// int Network_Size = sizeof(NICs);	// 218 at the moment
+	// std::string Network_List[10];
+	// for (int i = 0 ; i < Network_Size ; i++)
+	// {
+	// 	if(NICs[i] == '\n')
+	// 	{
+	// 		Networks = Networks.substr(0, Networks.find('p'));
+	// 		// Network_List[i] = Networks;
+	// 	}
+	// }
+/*
+############################################################
+END OF WORKING EXAMPLE
+############################################################
+*/
 
-	for (int i = 0 ; i < Network_Size ; i++)
-	{
-		if(NICs[i] == '\n')
-			std::cout << "GOTCHA" << std::endl;
-	}
+
+/*
+############################################################
+TRYING TO CATCH THE LAST NEW LINE; BUT THIS IS BEING A PAIN IN THE ASS
+############################################################
+*/
+
+	std::cout << Networks << std::endl;
+	std::cout << "==========================" << std::endl;
+	std::size_t found = Networks.find_last_of('\n');
+	std::string NewNetwork = Networks.substr(found-10);
+	std::cout << NewNetwork << std::endl;
+	// std::cout << Network_List[1] << std::endl;
 }
